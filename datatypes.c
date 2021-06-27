6c0cc859af306e7cd8e2253fe57b342977a49cee
Indupriya Selvam <indupriyaselvam@gmail.com>
2021-06-27 16:18:30 +0530

    Data types - format specifiers

---
+++ datatypes.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	unsigned int x, sub, a;
+	float y;
+	char z;
+	x = 3;
+	y = 4.5;
+	z = 'k';
+//	scanf("%u", &x);
+//	scanf("%u", &a);
+//	sub = x-a;
+	printf("The integer value is %u\n", x);
+	printf("The float value is %f\n", y);
+	printf("The charcater value is %c\n", z);
+//	printf("The sub value is %u\n", sub);
+	return 3;
+}
+
