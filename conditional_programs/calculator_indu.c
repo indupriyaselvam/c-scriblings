84e62e61b9c81c1456249cf7d40bda6634f54752
Indupriya Selvam <indupriyaselvam@gmail.com>
2021-06-27 16:32:52 +0530

    else if program & switch program for calculator

---
+++ conditional_programs/calculator_indu.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a, b, c;
+	char O;
+
+	printf("Enter the value: ");
+	scanf("%d", &a);
+	fflush(stdin);
+	printf("Enter the value: ");
+	scanf("%d", &b);
+	fflush(stdin);
+	printf("Operation: ");
+	scanf("%c", &O);
+	fflush(stdin);
+
+	if (O == '+'){
+		c = a+b;
+		printf("The addition value is %d\n", c);
+	} else if (O == '-'){
+		c = a-b;
+		printf("The sub value is %d\n", c);
+	} else if (O == '*'){
+		c = a*b;
+		printf("The multiply value is %d\n", c);
+	} else if (O == '/'){
+		c = a/b;
+		printf("The div value is %d\n", c);
+	} else {
+		printf("Only +, -, *, / operations can be performed with this calculator!\n");
+	}
+
+	return 0;
+}
---
+++ conditional_programs/calculator_indu_with_switch.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	char O;
+
+	printf("Enter the value: ");
+	scanf("%d", &a);
+	fflush(stdin);
+	printf("Enter the value: ");
+	scanf("%d", &b);
+	fflush(stdin);
+	printf("Operation: ");
+	scanf("%c", &O);
+	fflush(stdin);
+
+	switch (O) {
+		case '+':
+		printf("The addition value is %d\n", a+b);
+		break;										/* break is used to exit from switch and used to exit from loop */
+		case '-':
+		printf("The sub value is %d\n", a-b);
+		break;
+	    case '*':
+		printf("The multiply value is %d\n", a*b);
+		break;
+		case '/':
+		printf("The div value is %d\n", a/b);
+		break;
+		default:
+		printf("Only +, -, *, / operations can be performed with this calculator!\n");
+	}
+
+	return 0;
+}
